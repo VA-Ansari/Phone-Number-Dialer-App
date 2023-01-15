c591193accf6db30e2fc0d8bb930b006b0ede7ea
arshad ansari <arshadchennaisbg.apj.369@gmail.com>
2023-01-15 14:35:16 +0530

    version --2.0 directly calling app, Updated to ask user permission before calling

--- app/src/main/java/com/example/phonedialerapp/MainActivity.java
+++ app/src/main/java/com/example/phonedialerapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.phonedialerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -11,23 +15,40 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
+    EditText number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText number = findViewById(R.id.editnumber);
-        Button btn = findViewById(R.id.dialButton);
+        number = findViewById(R.id.editnumber);
+        btn = findViewById(R.id.dialButton);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String telNo = number.getText().toString();
-                Uri uri = Uri.parse("tel:"+telNo);
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
+                //checking if the permission is present or not
+                Toast.makeText(MainActivity.this, "HII", Toast.LENGTH_SHORT).show();
+
+                //these two parameters are for checking whether CALL_PHONE(From Manifest.permissions file) permission is present in this main activity or not.
+                int perm = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE);
+
+                if (perm == PackageManager.PERMISSION_GRANTED)
+                    callNumber();
+                else
+                    //else ask the user to give the permission
+                    //in parameters we can ask user for multiple permission from this activity and in last some random integer
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{ Manifest.permission.CALL_PHONE }, 121);
+
             }
         });
     }
+        void callNumber(){
+            String telNo = number.getText().toString();
+            Uri uri = Uri.parse("tel:"+telNo);
+            Intent intent = new Intent(Intent.ACTION_CALL, uri);  //Implicit intent
+            startActivity(intent);
+        }
 }
